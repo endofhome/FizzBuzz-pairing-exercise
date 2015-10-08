968266e2df55f85577fdd3ddbee64731ce66c5b1
Tom Barnes <ruralmyth@gmail.com>
2015-10-08 11:36:32 +0100

    passes Fizz and Buzz tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz(number)
-  'fizz'
+  if number == 15
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else 
+    number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,32 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do 
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it 'returns "Fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
   end
+
+  it 'return "Buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+
+  it 'return "FizzBuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'FizzBuzz'
+  end
+
+  it 'return 4 when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+  end
+
+  it 'return "Fizz" when passed 27' do
+    expect(fizzbuzz(27)).to eq 'Fizz'
+  end
+
+ it 'return "Fizz" when passed 55' do
+    expect(fizzbuzz(55)).to eq 'Buzz'
+  end
+
+ it 'return "Fizz" when passed 30' do
+    expect(fizzbuzz(30)).to eq 'FizzBuzz'
+  end
+
 end
